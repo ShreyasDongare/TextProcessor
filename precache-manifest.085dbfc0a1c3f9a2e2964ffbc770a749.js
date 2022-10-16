a75948c0d1e6d8fd81dfa9587573214897a91146
ShreyasDongare <dongareshreyas@gmail.com>
2022-10-16 20:39:32 +0530

    Updates

---
+++ precache-manifest.085dbfc0a1c3f9a2e2964ffbc770a749.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1eb12ac0c42884e795c",
+    "url": "/TextProcessor/static/css/main.86b8f104.chunk.css"
+  },
+  {
+    "revision": "c1eb12ac0c42884e795c",
+    "url": "/TextProcessor/static/js/main.c1eb12ac.chunk.js"
+  },
+  {
+    "revision": "02e73066ff27f2a42269",
+    "url": "/TextProcessor/static/js/1.02e73066.chunk.js"
+  },
+  {
+    "revision": "f78d312c0403105f8ae1",
+    "url": "/TextProcessor/static/js/runtime~main.f78d312c.js"
+  },
+  {
+    "revision": "a3d2c5d86212d08a2b0c900f470b3ac3",
+    "url": "/TextProcessor/index.html"
+  }
+];
